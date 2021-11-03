3d64c3e9c14a771b3d6bd94511067f69d76e2010
Adam Turner <hello@adamrichardturner.dev>
2021-11-03 19:22:54 +0000

    Re-factored to reduce code repetition in if statement sanitizing factors at the end

--- isPrime.py
+++ isPrime.py
@@ -1,13 +1,17 @@
 def isPrime(num):
     """
     Takes an integer as an input and checks whether the num is a prime
-    number, returning True or not (False). 
+    number, returning True or not if not prime (False). 
 
     A prime number is an integer that is greater than 1 and that has no
     positive divisors other than 1 and itself.
 
     Note: 1 is not a prime. 2, 3, 5, 7, 11, 13, 17, 19 are the prime numbers 
     less than 20.
+
+    Params: Num (Integer) which we test for being prime or not.
+
+    Returns: True if the number is prime, false if not.
     """
     # We will store our factors here and sanitise it at the end.
     factors = []
@@ -17,15 +21,12 @@ def isPrime(num):
         # we append it to factors.
         if num % i == 0:
             factors.append(i)
-    # If the length of factors is greater than 2, we don't have a prime number
-    if len(factors) > 2:
-        return False
-    # If the length of factors is 1, the only factor will be 1 so we return False.
-    elif len(factors) == 1:
-        return False
-    # Else if the factors contain only 2 members, we have a prime number.
-    else:
+    # Now we sanitize factors. If there are exactly 2 factors, we have a prime
+    # and return True. Else we do not, so we return False. 
+    if len(factors) == 2:
         return True
+    else:
+        return False
     
 print(isPrime(1)) # False
 print(isPrime(2)) # True
