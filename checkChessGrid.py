53c9190831f54ab21cc13ce38d512c857746e8d1
Adam Turner <hello@adamrichardturner.dev>
2021-11-20 22:59:28 +0000

    checkChessGrid complete

--- checkChessGrid.py
+++ checkChessGrid.py
@@ -1,12 +1,23 @@
 def checkChessGrid():
+    """
+    Checks chess coordinate given as user input is valid.
+
+    Params: None
+
+    Returns: Coordinates after validation (List)
+    """
     coordinate = input("Enter coord")
     if len(coordinate) == 2:
+    # If coordinate is correct length...(2)
         a2h = "a" <= coordinate[0] <= "h"
+        # Checks whether coordinate is between a and h(incl) and 1 and 8(incl)
         one2eight = "1" <= coordinate[1] <= "8"
         if a2h and one2eight:
+            # If a2h and one2eight are true, return the coordinates as a list..
             square = [coordinate[0].lower(), int(coordinate[1])]
             return square
         else:
-            return "Not a Chess Grid..."
+            # Otherwise, return error...
+            return "Not a Chess Grid Coordinate..."
     else:
-        print("Please input string with 2 chars")
+        return "Please input coordinate with 2 chars"
