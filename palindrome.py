e87ccc96b540105ba1c9bb257549fda70246b17b
Adam Turner <hello@adamrichardturner.dev>
2021-11-23 21:05:56 +0000

    Completed university lab

---
+++ palindrome.py
@@ -0,0 +1,27 @@
+def isPalindrome(content):
+    """
+    Takes an input and confirms whether it is an alphanumeric palindrome or not.
+
+    Params: content(Any)
+
+    Returns: True if the input is a alphanumeric palindrome, false if otherwise.
+    """
+    # Convert our input into a string in lowercase
+    content = str(content).lower()
+    # We will validate our content here...
+    valid = ""
+    # For each char in content...
+    for char in content:
+        # If the char is alphanumeric...
+        if char.isalnum():
+            # Concatenate with valid...
+            valid += char
+    # If valid is the same as valid reversed...
+    if valid[:] == valid[::-1]:
+        # Return True
+        return True
+    else:
+        # Else return False
+        return False
+
+print(isPalindrome("Madam, I'm Adam."))
