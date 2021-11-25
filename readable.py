013d72fb85273072016f19b3aa6dcc2bfc2adaf9
Adam Turner <hello@adamrichardturner.dev>
2021-11-25 21:57:53 +0000

    Added uni lab

--- readable.py
+++ readable.py
@@ -1,10 +1,25 @@
 def readable(phoneNum):
+    """
+    Validates and formats a phone number into a readable form with
+    brackets and hyphens to deleniate parts of the number.
+
+    Params: phoneNum(string) which should be 10 digits long...
+
+    Returns: The 10 digit phone number in a more readable form, e.g.
+    (415)555-1212
+    """
+    # Validate the phone number, if not 10 digits in length...
     if len(phoneNum) != 10:
+        # Print error...
         print("Phone number must be 10 digit long")
+    # Validate numbers have been entered, if not decimal numbers...
     elif not phoneNum.isdecimal():
+        # Print error...
         print("Input must be all digits")
+    # Format the phone number using string slicing to be more readable, separating
+    # digits of the phone number with brackets and hyphen.
     else:
-        return "(" + phoneNum[0:3] + ")" + phoneNum[3:6] + "-" + phoneNum[6:]
+        return "(" + phoneNum[:3] + ")" + phoneNum[3:6] + "-" + phoneNum[6:]
 
 print(readable("4155551212")) # (415)555-1212
 print(readable("5656391452")) # (565)639-1452
